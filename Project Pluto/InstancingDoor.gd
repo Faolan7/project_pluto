c7f72fc2d7f9c84640114d62e8de5f8318a44aca
DamienRodriguez <drodriguez17@eagles.ewu.edu>
2020-10-18 23:49:49 -0700

    Door coordinates are now random.
    There will need to be a deep discussion on how the doors will function relative to player as things are created on the account that this is chaos, when we want controlled chaos. We need to know what doors are connected to rooms, and so on, so that this functions perfectly as intended.

--- Project Pluto/InstancingDoor.gd
+++ Project Pluto/InstancingDoor.gd
@@ -2,9 +2,39 @@ extends Node2D
 
 onready var doorsprite = preload("res://Door.tscn")
 
+var door_coordinates = [
+	Vector2(140, 160),
+	Vector2(340, 160),
+	Vector2(240, 60),
+	Vector2(240, 260)
+]
+
+
 func _ready():
-	var scene = doorsprite.instance()
-	scene.position = Vector2(240, 160)
-	add_child(scene)
 	
+	randomize()
+	door_coordinates.shuffle()
+	
+	var number_of_doors: int = determine_number_of_doors()
+	var door_array = fill_door_array(number_of_doors)
+	
+	for n in range(0, door_array.size()):
+		add_child(door_array[n])
+		
 	pass
+
+func determine_number_of_doors() -> int: 
+	return randi() % 4 + 1
+	
+	
+func fill_door_array(num_of_doors) -> Array:
+	
+	var returned_array: Array = []
+	
+	for n in range(0, num_of_doors):
+		var temp_door_array = [doorsprite.instance()]
+		
+		returned_array += temp_door_array
+		returned_array[n].position = door_coordinates[n]
+	
+	return returned_array
