53e8b8b69e2fb2ed476a4568d8dddb9a975bf5c8
DamienRodriguez <drodriguez17@eagles.ewu.edu>
2020-10-19 00:00:49 -0700

    Cleaned up code.
    Cleaned up the door instancing script.

--- Project Pluto/InstancingDoor.gd
+++ Project Pluto/InstancingDoor.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
 onready var doorsprite = preload("res://Door.tscn")
-
-var door_coordinates = [
+onready var door_coordinates = [
 	Vector2(140, 160),
 	Vector2(340, 160),
 	Vector2(240, 60),
@@ -10,31 +9,30 @@ var door_coordinates = [
 ]
 
 
-func _ready():
-	
+func _ready():	
 	randomize()
 	door_coordinates.shuffle()
 	
-	var number_of_doors: int = determine_number_of_doors()
-	var door_array = fill_door_array(number_of_doors)
-	
+	var door_array = fill_door_array(determine_number_of_doors())
 	for n in range(0, door_array.size()):
 		add_child(door_array[n])
-		
+	
+	
 	pass
+
 
 func determine_number_of_doors() -> int: 
 	return randi() % 4 + 1
-	
-	
+
+
 func fill_door_array(num_of_doors) -> Array:
-	
 	var returned_array: Array = []
 	
+	
 	for n in range(0, num_of_doors):
-		var temp_door_array = [doorsprite.instance()]
-		
-		returned_array += temp_door_array
-		returned_array[n].position = door_coordinates[n]
+		var temp_doorsprite = doorsprite.instance()
+		temp_doorsprite.position = door_coordinates[n]
+		returned_array += [temp_doorsprite]
+	
 	
 	return returned_array
