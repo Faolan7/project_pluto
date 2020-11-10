4c6cc149a7e5d1585f1c3738a20bfecbdca654fb
DamienRodriguez <drodriguez17@eagles.ewu.edu>
2020-11-10 00:07:21 -0800

    Movement implemented
    Cardinal movement implemented. Need to work on getting the weapon to attack only in the direction that the player is facing. But that is another requirement entirely.

--- Project Pluto/Scripts/Player.gd
+++ Project Pluto/Scripts/Player.gd
@@ -5,6 +5,10 @@ onready var state_machine: Node = $StateMachine
 onready var move_state: Node = $StateMachine/Move
 onready var attack_state: Node = $StateMachine/Attack
 
+onready var animation_player: Node = $AnimationPlayer
+onready var animation_tree: Node = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
 
 func _ready() -> void:
 	attack_state.weapon = $Weapons/Weapon as Weapon
@@ -21,4 +25,9 @@ func _unhandled_input(_event: InputEvent) -> void:
 		state_machine.change_state(attack_state)
 		
 	elif input_vector != Vector2.ZERO: # Checking if move button is pushed
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		state_machine.change_state(move_state)
+	else:
+		animation_state.travel("Idle")
