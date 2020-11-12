ed8b13f569498164e7912baebade36a2fc2f1e7d
DamienRodriguez <drodriguez17@eagles.ewu.edu>
2020-11-12 14:20:47 -0800

    Clean up

--- Project Pluto/Scripts/DroppedItem.gd
+++ Project Pluto/Scripts/DroppedItem.gd
@@ -7,13 +7,12 @@ extends Area2D
 
 
 
-# If we did this, we would have to preload every single 
-
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Weapon.visible = true
+
 
 func item_grabbed() -> Weapon:
 	return $Weapon as Weapon
