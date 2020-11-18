21d99bd66b819079d1c6a9ad781a813ec9944812
DamienRodriguez <drodriguez17@eagles.ewu.edu>
2020-11-18 15:03:12 -0800

    enemies now drop weapons
    weapon pick up functionality unaffected, enemies can now "drop" weapons.

--- Project Pluto/Scripts/DroppedItem.gd
+++ Project Pluto/Scripts/DroppedItem.gd
@@ -6,8 +6,12 @@ extends InteractBox
 # var b = "text"
 onready var weapon: Weapon = $Weapon
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$Weapon.visible = true
+
+func init(passed_weapon: Weapon, dropped_position: Vector2) -> void:
+	weapon = passed_weapon
+	add_child(weapon)
+	position = dropped_position
+	weapon.visible = true
 
 func interact(player: Player):
 	.interact(player)
--- Project Pluto/Scripts/Enemy/Enemy.gd
+++ Project Pluto/Scripts/Enemy/Enemy.gd
@@ -5,7 +5,8 @@ extends KinematicBody2D
 export(int) var health: int setget set_health
 
 onready var health_bar: HealthBar = $HealthBar as HealthBar
-
+onready var DROPPED_ITEM: Resource = load("res://Scenes/DroppedItem.tscn")
+onready var weapon: Weapon = $Weapon
 
 func _ready() -> void:
 	health_bar.max_health = health
@@ -15,6 +16,17 @@ func set_health(value: int) -> void:
 	health = value
 	
 	if health <= 0: # Checking if dead
+		#instance dropped_item
+		var dropped_weapon = DROPPED_ITEM.instance()
+		
+		#ole switcharoo
+		remove_child(weapon)
+		dropped_weapon.init(weapon, position)
+		
+		#add the dropped item as a child of the Dungeon
+		get_parent().get_parent().call_deferred("add_child", dropped_weapon)
+		
+		#remove the dead body
 		queue_free()
 		return
 		
