13791459bf69f75e4d59e1621526c76ea1cbb5d4
DamienRodriguez <drodriguez17@eagles.ewu.edu>
2021-01-26 16:07:36 -0800

    Matthew said so.

--- Project Pluto/Scripts/Interface/ResourceManager.gd
+++ Project Pluto/Scripts/Interface/ResourceManager.gd
@@ -3,9 +3,15 @@ extends Node
 func connect_player(player: Player):
 	player.connect('update_health', self, '_on_hp_update')
 	player.connect('update_stamina', self, '_on_stamina_update')
-	
+
+	$Health.max_value = player.get_max_health()
+	$Stamina.max_value = player.get_max_stamina()
+	$Stamina.visible = true
+
 func _on_hp_update(value: float) -> void:
+	print("Resources Current HP Value: ", value)
 	$Health.update_bar(value)
 
 func _on_stamina_update(value: float) -> void:
 	$Stamina.update_bar(value)
+
