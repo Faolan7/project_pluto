97f3b067a617f0741ba6925cce868e2d38503976
Faolan7 <matthew@themontneys.com>
2021-02-02 16:32:49 -0800

    Fix weapon slot sprite issue

--- Project Pluto/Scripts/Interface/WeaponSlot.gd
+++ Project Pluto/Scripts/Interface/WeaponSlot.gd
@@ -3,11 +3,16 @@ extends Sprite
 
 
 var weapon: Weapon setget _set_weapon
-
-onready var weapon_sprite: Sprite = $WeaponSprite as Sprite
+var weapon_sprite: Sprite
 
 
 func _set_weapon(value: Weapon) -> void:
-	weapon_sprite.texture = value.texture
-	weapon_sprite.modulate = value.modulate
-	weapon_sprite.self_modulate = value.self_modulate
+	if weapon_sprite != null:
+		weapon_sprite.queue_free()
+		
+	weapon_sprite = value.duplicate()
+	weapon_sprite.position = Vector2.ZERO
+	weapon_sprite.rotation = 0
+	weapon_sprite.visible = true
+	weapon_sprite.scale = weapon_sprite.scale * Vector2(1 / scale.x, 1 / scale.y)
+	add_child(weapon_sprite)
