9fe8b1fdfbbdeeafb08559f7a748757f371883e1
Faolan7 <matthew@themontneys.com>
2021-02-12 15:41:31 -0800

    Reimplement bow attacks

--- Project Pluto/Scripts/Weapon/Attacks.gd
+++ Project Pluto/Scripts/Weapon/Attacks.gd
@@ -4,16 +4,36 @@ extends Node
 # and the workaround would be dirty and annoying
 
 
-static func perform(attack: String, weapon) -> void:
+static func perform(attack: String, weapon, attack_dir: float) -> void:
 	weapon.visible = true
 	
 	match attack:
+		# Normal attacks
+		'shoot': shoot(weapon, attack_dir, 1)
 		'stab': stab(weapon)
+		# Special attacks
+		'spin': spin(weapon)
+		'spreadshot3': shoot(weapon, attack_dir, 3)
+		# Default
+		_: print('ERROR: Unknown attack ' + attack)
 
 static func _on_attack_finished(weapon, special: bool) -> void:
 	weapon.visible = false
 	weapon.emit_signal('attack_finished' if not special else 'special_finished')
 
+
+static func shoot(weapon, attack_dir: float, num_projectiles: int) -> void:
+	var cone_size: float = (num_projectiles - 1) / 20.0 # Doing floating point division
+	var angle_diff: float = -cone_size
+	
+	weapon.animation_player.play('shoot')
+	while angle_diff <= cone_size:
+		weapon.create_projectile(attack_dir + angle_diff)
+		
+		angle_diff += .1
+		
+	yield(weapon.animation_player, 'animation_finished')
+	_on_attack_finished(weapon, false)
 
 static func stab(weapon) -> void:
 	var STAB_DURATION: float = .1
@@ -25,3 +45,7 @@ static func stab(weapon) -> void:
 	
 	yield(weapon.tween, 'tween_completed')
 	_on_attack_finished(weapon, false)
+
+
+static func spin(_weapon) -> void:
+	pass
