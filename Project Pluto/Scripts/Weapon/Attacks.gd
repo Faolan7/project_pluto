b95ebbd52f567c909d1acdd1e77cb2fb924d59de
Faolan7 <matthew@themontneys.com>
2021-02-26 16:58:56 -0800

    Fix lightning attack

--- Project Pluto/Scripts/Weapon/Attacks.gd
+++ Project Pluto/Scripts/Weapon/Attacks.gd
@@ -8,14 +8,14 @@ static func perform(attack: String, weapon, special: bool, attack_dir: float) ->
 	weapon.visible = true
 	
 	match attack:
+		'fireball': fireball(weapon, special)
+		'lightning': lightning(weapon, special)
 		'shoot': shoot(weapon, special, attack_dir, 1)
 		'slam': slam(weapon, special)
 		'spin': swing(weapon, special, PI)
 		'spreadshot3': shoot(weapon, special, attack_dir, 3)
 		'stab': stab(weapon, special)
 		'swing': swing(weapon, special, PI / 2)
-		'explode': explode(weapon, special)
-		'sparks': stab(weapon, special)
 		_: print('ERROR: Unknown attack ' + attack)
 
 static func _on_attack_finished(weapon, special: bool) -> void:
@@ -24,20 +24,25 @@ static func _on_attack_finished(weapon, special: bool) -> void:
 	weapon.emit_signal('attack_finished' if not special else 'special_finished')
 
 
-static func slam(weapon, special: bool) -> void:
-	weapon.set_hitbox_enabled(true, special)
-	
-	yield(weapon.get_tree().create_timer(.15), 'timeout')
-	_on_attack_finished(weapon, special)
-
-
-static func explode(weapon, special: bool) -> void:
+static func fireball(weapon, special: bool) -> void:
 	weapon.set_hitbox_enabled(true, special)
 	weapon.set_hitbox_position_to_mouse(special)
 	
 	yield(weapon.get_tree().create_timer(.15), 'timeout')
 	_on_attack_finished(weapon, special)
 
+static func lightning(weapon, special: bool) -> void:
+	weapon.animation_player.play('lightning')
+	weapon.set_hitbox_enabled(true, special)
+	
+	yield(weapon.animation_player, 'animation_finished')
+	_on_attack_finished(weapon, special)
+
+static func slam(weapon, special: bool) -> void:
+	weapon.set_hitbox_enabled(true, special)
+	
+	yield(weapon.get_tree().create_timer(.15), 'timeout')
+	_on_attack_finished(weapon, special)
 
 static func shoot(weapon, special: bool, attack_dir: float, num_projectiles: int) -> void:
 	var cone_size: float = (num_projectiles - 1) / 20.0 # Doing floating point division
@@ -57,16 +62,6 @@ static func stab(weapon, special: bool) -> void:
 		Vector2(2, 0), weapon.position, .1)
 		
 	yield(weapon.tween, 'tween_completed')
-	_on_attack_finished(weapon, special)
-	
-static func sparks(weapon, special: bool) -> void:
-	weapon.animation_player.play('Tome#1')
-	weapon.set_hitbox_enabled(true, special)
-	weapon.play_tween(weapon, 'position',
-		Vector2(2, 0), weapon.position, .1)
-		
-	yield(weapon.tween, 'tween_completed')
-	yield(weapon.animation_player, 'animation_finished')
 	_on_attack_finished(weapon, special)
 
 static func swing(weapon, special: bool, attack_angle: float) -> void:
