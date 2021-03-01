bba8a44baa15bde4fe52ffa306906e182b89634b
RandyHec <Randy.80@hotmail.com>
2021-03-01 15:05:54 -0800

    fireball causing targeting

--- Project Pluto/Scripts/Weapon/Hitbox.gd
+++ Project Pluto/Scripts/Weapon/Hitbox.gd
@@ -2,8 +2,12 @@ class_name Hitbox
 extends Area2D
 
 
-var entity: PhysicsBody2D
+var entity: PhysicsBody2D setget set_entity
 
 onready var shape: CollisionShape2D = $CollisionShape2D as CollisionShape2D
 
 export(int) var damage: int = 0
+
+
+func set_entity(value: PhysicsBody2D) -> void:
+	entity = value
--- Project Pluto/Scripts/Weapon/ProjectileLibrary/Fireball.gd
+++ Project Pluto/Scripts/Weapon/ProjectileLibrary/Fireball.gd
@@ -2,6 +2,12 @@ extends Projectile
 
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer as AnimationPlayer
+onready var explosion_hitbox: Hitbox = $ExplosionHitbox as Hitbox
+
+
+func set_entity(value: PhysicsBody2D) -> void:
+	.set_entity(value)
+	explosion_hitbox.entity = entity
 
 
 func _on_collision(hurtbox: HurtBox) -> void:
