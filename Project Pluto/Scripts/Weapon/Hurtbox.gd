281102f5915d14319061cffb18adf6220426331f
Faolan7 <matthew@themontneys.com>
2021-03-02 14:54:08 -0800

    Implement attack strength

--- Project Pluto/Scripts/Weapon/Hurtbox.gd
+++ Project Pluto/Scripts/Weapon/Hurtbox.gd
@@ -12,4 +12,6 @@ func _on_area_entered(area: Area2D) -> void:
 	if area is Hitbox and area.entity != entity:
 		var hitbox: Hitbox = area as Hitbox
 		
-		emit_signal('damaged', hitbox.damage, hitbox.entity)
+		emit_signal('damaged',
+			hitbox.damage * hitbox.entity.damage_multiplier,
+			hitbox.entity)
